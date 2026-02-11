336dad10c6f70dc99a6e145cde2f24a0fb680f57
Amina Sabit <sabitamina811@gmail.com>
2026-02-11 15:29:27 +0500

    Commit

---
+++ lab3/birthday.js
@@ -0,0 +1,2 @@
+const BIRTHDAY = '29.06.2007';
+const age = someCode(BIRTHDAY);
---
+++ lab3/string.js
@@ -0,0 +1,5 @@
+let name = "Ilya";
+
+alert (`Hello ${1}`); //Hello 1
+alert (`Hello ${"name"}`); //Hello name
+alert (`Hello ${name}`); //Hello Ilya
